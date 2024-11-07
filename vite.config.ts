f4d435ed69b7a77b2f3989f7f41dcf390d82641d
kevin-rojas17 <kevin.rojascuritima@unas.edu.pe>
2024-11-07 15:13:08 -0500

    s

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vistas-fiis/', // Agrega aquí el nombre del repositorio
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
